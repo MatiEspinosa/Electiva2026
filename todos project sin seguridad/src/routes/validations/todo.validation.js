8f193893b285f6f620d54077119278580d7092c0
Matias Espinosae <chinoespinosa012@gmail.comm>
2026-04-26 18:40:41 -0300

    Elimino todo

--- todos project sin seguridad/src/routes/validations/todo.validation.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-
-const todoSchema = Joi.object({
-  title: Joi.string().min(3).max(20).required(),
-  description: Joi.string().min(3).max(20).required()
-});
-
-module.exports = todoSchema;
